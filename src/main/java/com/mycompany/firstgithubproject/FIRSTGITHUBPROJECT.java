a4b4563549ac61411197e9348a209b5c7dd1deaf
MOFENYI <MOFENYI@Mofenyi>
2024-09-09 10:23:11 +0200

    created a java program that adds two numbers

--- src/main/java/com/mycompany/firstgithubproject/FIRSTGITHUBPROJECT.java
+++ src/main/java/com/mycompany/firstgithubproject/FIRSTGITHUBPROJECT.java
@@ -11,6 +11,9 @@ package com.mycompany.firstgithubproject;
 public class FIRSTGITHUBPROJECT {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a = 5;
+        int b = 5;
+        int c = a + b;
+        System.out.println("Sum :"  + c);
     }
 }
